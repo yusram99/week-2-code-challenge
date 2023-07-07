c66c89a7158fd0f27c7f5558958fcd778f6587e8
Your Name <youremail@example.com>
2023-07-07 04:13:55 +0300

    commit message

---
+++ README.md
@@ -0,0 +1,27 @@
+# Flataculties - Vote for the Cutest Animal
+Flataculties is a web application where users can vote for the cutest animal from a list of options. Users can view the list of animals, click on an animal to see its details including the number of votes it has received, and add new animals to the list. The application allows users to reset the votes for all animals.
+
+# Features
+View a list of animals and their names
+Click on an animal to view its details, including the image and the number of votes
+Add new animals to the list
+Reset the votes for all animals
+Technologies Used
+HTML
+CSS
+JavaScript
+
+# Usage
+Click on an animal name in the list to view its details.
+The details section will show the animal's name, an image, and the number of votes it has received.
+To add a new animal, enter its name in the "Animal Name" input field and click "Add Animal".
+To reset the votes for all animals, click the "Reset Votes" button.
+Contributing
+Contributions to the Flataculties app are welcome! If you encounter any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+# License
+The Flataculties app is open source and available under the MIT License.
+
+# Acknowledgements
+The Flataculties app was inspired by the concept of voting for the cutest animal.
+The sample animal data used in the app is sourced from various GIF and image websites.
--- index.js
+++ index.js
@@ -1,62 +1,65 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // JSON data
-    const charactersData = {
-      "characters": [
-        {
-          "id": 1,
-          "name": "Mr. Cute",
-          "image": "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif",
-          "votes": 0
-        },
-        {
-          "id": 2,
-          "name": "Mx. Monkey",
-          "image": "https://thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif",
-          "votes": 0
-        },
-        {
-          "id": 3,
-          "name": "Ms. Zebra",
-          "image": "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif",
-          "votes": 0
-        },
-        {
-          "id": 4,
-          "name": "Dr. Lion",
-          "image": "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif",
-          "votes": 0
-        },
-        {
-          "id": 5,
-          "name": "Mme. Panda",
-          "image": "https://media.giphy.com/media/ALalVMOVR8Qw/giphy.gif",
-          "votes": 0
-        }
-      ]
-    };
+// Sample animal data
+const animals = [
+    { id: 1, name: "Mr. Cute", image: "https://thumbs.gfycat.com/EquatorialIckyCat-max-1mb.gif", votes: 0 },
+    { id: 2, name: "Mx. Monkey", image: "https://thumbs.gfycat.com/FatalInnocentAmericanshorthair-max-1mb.gif", votes: 0 },
+    { id: 3, name: "Ms. Zebra", image: "https://media2.giphy.com/media/20G9uNqE3K4dRjCppA/source.gif", votes: 0 },
+    { id: 4, name: "Dr. Lion", image: "http://bestanimations.com/Animals/Mammals/Cats/Lions/animated-lion-gif-11.gif", votes: 0 },
+    { id: 5, name: "Mme. Panda", image: "https://media.giphy.com/media/ALalVMOVR8Qw/giphy.gif", votes: 0 },
+  ];
   
-    // Get the characterList container element
-    const characterList = document.getElementById("characterList");
+  // Function to populate the animal list
+  function populateAnimalList() {
+    const animalList = document.getElementById("animal-list");
   
-    // Loop through the characters and create elements to display them
-    charactersData.characters.forEach(character => {
-      // Create a div element for each character
-      const characterDiv = document.createElement("div");
-  
-      // Create an image element and set its source
-      const characterImage = document.createElement("img");
-      characterImage.src = character.image;
-  
-      // Create a heading element and set its text content
-      const characterName = document.createElement("h3");
-      characterName.textContent = character.name;
-  
-      // Append the image and heading elements to the character div
-      characterDiv.appendChild(characterImage);
-      characterDiv.appendChild(characterName);
-  
-      // Append the character div to the characterList container
-      characterList.appendChild(characterDiv);
+    animals.forEach(animal => {
+      const listItem = document.createElement("li");
+      listItem.textContent = animal.name;
+      listItem.addEventListener("click", () => showAnimalDetails(animal.id));
+      animalList.appendChild(listItem);
     });
-  });
+  }
   
+  // Function to show the animal details
+  function showAnimalDetails(animalId) {
+    const animalDetails = document.getElementById("animal-details");
+    const animal = animals.find(animal => animal.id === animalId);
+  
+    animalDetails.innerHTML = `
+      <h3>${animal.name}</h3>
+      <img src="${animal.image}" alt="${animal.name}">
+      <p>Votes: ${animal.votes}</p>
+    `;
+  }
+  
+  // Function to add votes for an animal
+  function addVotes(animalId) {
+    const animal = animals.find(animal => animal.id === animalId);
+    animal.votes++;
+    showAnimalDetails(animalId);
+  }
+  
+  // Function to reset votes for all animals
+  function resetVotes() {
+    animals.forEach(animal => animal.votes = 0);
+    showAnimalDetails(1); // Show details of the first animal
+  }
+  
+  // Function to handle form submission
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const animalNameInput = document.getElementById("animal-name");
+    const animalName = animalNameInput.value;
+    const animalId = animals.length + 1; // Generate a new unique ID
+    const newAnimal = { id: animalId, name: animalName, votes: 0 };
+    animals.push(newAnimal);
+    animalNameInput.value = ""; // Reset input field
+    populateAnimalList();
+  }
+  
+  // Event listeners
+  document.getElementById("reset-button").addEventListener("click", resetVotes);
+  document.getElementById("add-animal-form").addEventListener("submit", handleFormSubmit);
+  
+  // Initialize the application
+  populateAnimalList();
+  showAnimalDetails(1); // Show details of the first animal
